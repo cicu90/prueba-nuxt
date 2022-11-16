166303f57d299d37a2ea9dbe46e8e23772d4d6ee
cicu90 <carlacardozo990@gmail.com>
2022-11-16 15:57:25 +0100

    connection with api/spotify

---
+++ components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form>
+      <input v-model="queryName" type="text" name="find" placeholder="Search you artists here" />
+      <button @click.prevent="getArtists">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySearch',
+  data () {
+    return {
+      queryName: '',
+      todoId: 1,
+      response: {},
+      artists: []
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      const response = await this.$axios.$get('/server-middleware/login'
+      ).then(response => response)
+      console.log('response', response)
+    },
+    async getArtists () {
+      const response = await this.$axios.$get(`/server-middleware/artists/${this.queryName}`
+      ).then(response => response)
+      console.log('response', response)
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <search/>
+</template>
+
+<script>
+import Search from "~/components/Search.vue"
+export default {
+  name: 'IndexPage',
+  components: {
+    Search,
+  },
+  data () {
+    return {
+      name: '',
+      pageSize: 20
+    }
+  },
+}
+</script>
